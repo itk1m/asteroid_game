1efc4e6b2436efdc83701d853aca4a1d7efe0aa8
Isaac <isaactaekim987@gmail.com>
2025-10-23 13:00:54 -0400

    Added changes to change the direction of the user when they press the a or d key

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from circleShape import CircleShape
+from circleshape import CircleShape
 
 
 class Player(CircleShape):
@@ -19,3 +19,13 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
